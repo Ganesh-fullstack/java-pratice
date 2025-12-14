e2b55f34a257c15040dba3f8fbbf15fbf5c342d1
P.Ganesh <changalerayudup@gmail.com>
2025-12-14 11:42:49 +0530

    Add files via upload

---
+++ Even.java
@@ -0,0 +1,15 @@
+class Even{
+public static void main(Strings args[]){
+
+   Scanner sc=new Scanner(System.in); 
+   System.out.println("Enter Numbers");
+  int a=sc.Int();
+    if((a/2)==0){
+    System.out.println("Even Number");
+    }
+    else{
+   System.out.println("Odd Number");
+
+}
+ }
+}
