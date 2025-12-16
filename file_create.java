ef64ccb7d18383cf8dbfaaa8b270ee853aea1e12
P.Ganesh <changalerayudup@gmail.com>
2025-12-16 07:26:38 +0530

    Add files via upload

---
+++ file_create.java
@@ -0,0 +1,11 @@
+import java.io.File;
+import java.io.IOException;
+
+class FileReader{
+  public static void main(String args[]) throw IOException{
+
+
+   File file=new File("E:/GANESH/Resume.txt");
+   System.out.println(createNewFile());
+ }
+}
